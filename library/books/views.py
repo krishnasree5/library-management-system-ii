5d5c6c6bebf7ad8df7940dc65a5b2eac24f6572b
{Krishna <gkrishnasree5@gmail.com>
2024-06-14 13:54:38 +0530

    Fix styles

--- library/books/views.py
+++ library/books/views.py
@@ -14,7 +14,8 @@ def search(request):
         books = books.filter(category=category_id)    
     if query:
         books = books.filter(Q(title__icontains=query)|Q(isbn__icontains=query)|Q(author__icontains=query)|Q(publisher__icontains=query))
-    
+    else:
+        books = books[:9]
 
     return render(request, 'books/search.html', {
         'books': books,
